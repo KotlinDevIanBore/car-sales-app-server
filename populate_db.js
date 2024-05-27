9ac34bf489ece577e4d184d91e4bff3a924611bb
KotlinDevIanBore <iankipchirchirb3@gmail.com>
2024-05-27 09:53:28 +0300

    Made the functionality of moving images to the database

--- populate_db.js
+++ populate_db.js
@@ -1,36 +1,32 @@
-import mysql from 'mysql2/promise';
-import CARS from './Data.js';
-// import dotenv from 'dotenv';
+import mysql from "mysql2/promise";
+import CARS from "./Data.js";
+async function dbConnection() {
+  const dbConfig = {
+    host: "localhost",
+    user: "root",
+    password: "ianbore12",
+    database: "cars",
+  };
 
-// dotenv.config();
-
-// const dbConfig = {
-
-//   host: process.env.DB_host,
-//   user: process.env.DB_user,
-//   password: process.env_DB_PASSWORD, 
-//   database: process.env_DB_DATABASE,
-
-// };
-
-const dbConfig = {
-    host:"localhost",
-    user:"root",
-    password:"ianbore12",
-    database:"cars"
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    return connection;
+  } catch (error) {
+    console.error("connection to the database failed", error);
+    throw error;
+  }
 }
 
-async function populateDatabase (){
+async function populateDatabase() {
+  try {
+    const connection = await dbConnection();
 
-try {
-const connection = await mysql.createConnection (dbConfig);
-console.log ('connection to the database was successful')
+    Promise.all(
+      CARS.map(async (car) => {
+        const { id, brand, name, imageIndex, price, availability, location } =
+          car;
 
-Promise.all(CARS.map (async(car)=>{
-
-    const {id,brand,name,imageIndex,price,availability,location}=car;
-
-    const query = `INSERT INTO car_schema (id,brand,name,imageIndex,price,availability,location) VALUES (?,?,?,?,?,?,?) 
+        const query = `INSERT INTO car_schema (id,brand,name,imageIndex,price,availability,location) VALUES (?,?,?,?,?,?,?) 
     
     ON DUPLICATE KEY UPDATE
     brand = VALUES(brand),
@@ -40,51 +36,56 @@ Promise.all(CARS.map (async(car)=>{
     availability = VALUES(availability),
     location = VALUES(location)
     `;
-    await connection. execute(query, [id, brand, name, imageIndex,price, availability, location] )
-
-
-
-}))
-
-
-
-
-}
-catch (error){
-    console.log ('Could not make a connection to the database');
-
-}
-
-}
-
-async function populateImages (){
-
-    const connection = await mysql.createConnection (dbConfig);
-
-
-    Promise.all (CARS.map (async(car)=>{
-
-       const {id,image}= car;
-
-       Promise.all (
-        image.forEach (async (eachImage)=>{
-            const populateImageQuery = `INSERT INTO car_images(id,URL) VALUES(?,?)`;
-    
-            try {
-            await connection.execute (populateImageQuery, [id,eachImage.URL])
-           }
-           catch (error){
-            console.log ('Failure to populate images to the database');
-            connection.end;
-           }
-    
-           })
-       )
-        
-       }))
-
-    
+        await connection.execute(query, [
+          id,
+          brand,
+          name,
+          imageIndex,
+          price,
+          availability,
+          location,
+        ]);
+      })
+    );
+  } catch (error) {
+    console.log("Could not populate carsto the database");
+  }
 }
 
-populateDatabase();
-populateImages();
+async function populateImages() {
+  const connection = await dbConnection();
+
+  await Promise.all(
+    CARS.map(async (car) => {
+      const { id, image } = car;
+
+      await Promise.all(
+        image.map(async (eachImage) => {
+          const populateImageQuery = `INSERT INTO car_images(car_id,URL) VALUES(?,?)
+            
+            
+            `;
+
+          try {
+            await connection.execute(populateImageQuery, [id, eachImage.URL]);
+          } catch (error) {
+            console.error(`Failed to insert image for car ${id}:`, error);
+          }
+        })
+      );
+    })
+  );
+  return "Populate Images Successful";
+}
+
+(async () => {
+  try {
+    await populateImages();
+    await populateDatabase();
+  } catch (error) {
+    console.error("Error in populating images:", error);
+  } finally {
+    const connection = await dbConnection();
+    connection.end;
+  }
+})();
