f9c5bf487d0d3d153860c8c1193945a3237ffb2d
KotlinDevIanBore <iankipchirchirb3@gmail.com>
2024-08-02 13:26:18 +0300

    Rewrote db.mjs

--- models/db.mjs
+++ models/db.mjs
@@ -1,56 +1,41 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
-import { config } from 'dotenv';
+import fs from 'fs';
 
-dotenv.config();
-config({ path: './car.env' }); 
-config();
-import fs from 'fs'; 
-
-
-
+dotenv.config({ path: './car.env' });
 
 const dbConfig = {
-
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database:process.env.DB_DATABASE,
-    port: process.env.DB_PORT ,
-    ssl: {
-        ca: fs.readFileSync('./client-cert.pem'), 
-        key: fs.readFileSync('./client-key.pem') ,
-        rejectUnauthorized: false 
-    }
-
-}
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+  ssl: {
+    ca: fs.readFileSync('./client-cert.pem'),
+    key: fs.readFileSync('./client-key.pem'),
+    rejectUnauthorized: false, // Consider setting this to true
+  },
+};
 
 const connectionPool = mysql.createPool(dbConfig);
 
-
- async function getConnection (){
-
-    try {
-
-        const connection = await connectionPool.getConnection();
-        return connection;    
-    }catch (error){
-    
-        console.error ('connection to database failed', error)
-        throw new Error('Failed to get connection from pool'); // Re-throw a new error
-      }
-
-    
+async function getConnection() {
+  try {
+    const connection = await connectionPool.getConnection();
+    return connection;
+  } catch (error) {
+    console.error('Connection to database failed:', error);
+    throw new Error('Failed to get connection from pool');
+  }
 }
 
-async function closeConnection (connection){
-    try {
-       await connection.release();
-      } catch (error) {
-        console.error('Error releasing connection:', error);
-        throw error; // Re-throw the error
-
-      }
+async function closeConnection(connection) {
+  try {
+    await connection.release();
+  } catch (error) {
+    console.error('Error releasing connection:', error);
+    throw error;
+  }
 }
 
 export { getConnection, closeConnection };
