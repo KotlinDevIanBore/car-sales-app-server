80822c1fc88b59a957edd93ad588c32cb97c2ca3
KotlinDevIanBore <iankipchirchirb3@gmail.com>
2024-08-02 09:13:09 +0300

    (chore)Added spacing to process env

--- index.mjs
+++ index.mjs
@@ -17,7 +17,7 @@ import recieveCarEditsRouter from "./routes/editcar.mjs";
 
 const app = express();
 
-const port = process.env.PORT||3000;
+const port = process.env.PORT || 3000;
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
